f2aaaff5656a5e2eacdb60b3af69be3304105240
melihburakmert <melih.burak.mert@gmail.com>
2025-04-26 15:02:31 +0200

    Add modulith IT

--- src/test/java/mbm/brokerage_backend/BrokerageBackendApplicationTests.java
+++ src/test/java/mbm/brokerage_backend/BrokerageBackendApplicationTests.java
@@ -2,12 +2,22 @@ package mbm.brokerage_backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.modulith.core.ApplicationModules;
+import org.springframework.modulith.docs.Documenter;
 
 @SpringBootTest
 class BrokerageBackendApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void writeDocumentationSnippets() {
+		final var modules = ApplicationModules.of(BrokerageBackendApplication.class).verify();
+		modules.forEach(System.out::println);
+
+		new Documenter(modules).writeModulesAsPlantUml().writeIndividualModulesAsPlantUml();
 	}
 
 }
