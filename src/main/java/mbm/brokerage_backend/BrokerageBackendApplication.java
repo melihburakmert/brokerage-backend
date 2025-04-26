b2dd88a3f6cdc096532f0f6addb46f2a73116be1
melihburakmert <melih.burak.mert@gmail.com>
2025-04-26 12:19:57 +0200

    Initial commit

---
+++ src/main/java/mbm/brokerage_backend/BrokerageBackendApplication.java
@@ -0,0 +1,13 @@
+package mbm.brokerage_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrokerageBackendApplication {
+
+	public static void main(final String[] args) {
+		SpringApplication.run(BrokerageBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mbm/brokerage_backend/BrokerageBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package mbm.brokerage_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrokerageBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
