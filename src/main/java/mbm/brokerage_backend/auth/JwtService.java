6e87af90216df2c0179d9f9143b4866016ec5c1f
melihburakmert <melih.burak.mert@gmail.com>
2025-04-30 20:31:06 +0200

    Add Jwt service

---
+++ src/main/java/mbm/brokerage_backend/auth/JwtService.java
@@ -0,0 +1,12 @@
+package mbm.brokerage_backend.auth;
+
+import org.springframework.security.core.userdetails.UserDetails;
+
+
+public interface JwtService {
+    String extractUsername(String token);
+
+    String generateToken(UserDetails userDetails);
+
+    boolean isTokenValid(String token, UserDetails userDetails);
+}
